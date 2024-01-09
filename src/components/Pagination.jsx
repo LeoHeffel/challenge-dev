16e169ed24f59a6b6a54c01402e3c24468e32c9c
LeoHeffel <leo_heffel@outlook.com>
2024-01-09 19:44:42 -0300

    fix : styles

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -17,10 +17,10 @@ const Pagination = () => {
     return (
       <div className="flex flex-col items-center my-4">
         {/* Help text */}
-        <span className="text-sm text-gray-700 dark:text-gray-400">
+        <span className="text-sm text-gray-700 text-gray-400">
           Showing <span className="font-semibold text-white">{page}</span> of{" "}
           <span className="font-semibold text-white">{pages}</span> of{" "}
-          <span className="font-semibold text-gray-900 dark:text-white"></span>{" "}
+          <span className="font-semibold text-gray-900 text-white"></span>{" "}
           Pages
         </span>
         <div className="inline-flex mt-2 xs:mt-0">
@@ -30,7 +30,7 @@ const Pagination = () => {
             onClick={() => setPage(prev)}
             className={`${
               page <= 1 ? "cursor-not-allowed " : ""
-            } flex  items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+            } flex  items-center justify-center px-4 h-10 text-base font-medium border-0 border-e  rounded-s  bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white`}
           >
             <svg
               className="w-3.5 h-3.5 me-2 rtl:rotate-180"
@@ -54,7 +54,7 @@ const Pagination = () => {
             onClick={() => setPage(next)}
             className={`${
               page >= pages ? "cursor-not-allowed " : ""
-            } flex  items-center justify-center px-4 h-10 text-base font-medium text-white bg-gray-800 border-0 border-s border-gray-700 rounded-e hover:bg-gray-900 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white`}
+            } flex  items-center justify-center px-4 h-10 text-base font-medium  border-0 border-s  rounded-e  bg-gray-800 border-gray-700 text-gray-400 hover:bg-gray-700 hover:text-white`}
           >
             Next
             <svg
